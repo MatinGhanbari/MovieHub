b22ee9e1d6705de04720f676b64306df253ade19
Matin_Ghanbari <ghanbarimatin6@gmail.com>
2025-01-08 23:45:20 +0330

    initial commit

---
+++ src/index.js
@@ -0,0 +1,113 @@
+async function fetchWithToken(title) {
+    try {
+        let url = `https://www.omdbapi.com/?apikey=abb7cdf7&s=${encodeURIComponent(title)}`;
+
+        const response = await fetch(url);
+        const data = await response.json();
+        const resultsContainer = document.getElementById("results");
+        resultsContainer.innerHTML = "";
+
+        if (data.Response === "True") {
+            let moviesHtml = '<div class="row">';
+            data.Search.forEach((movie) => {
+                const poster = movie.Poster !== "N/A" ? movie.Poster : "/assets/images/default.png";
+                const imdbID = movie.imdbID.replace("tt", "");
+
+                moviesHtml += `
+                            <div class="col-6 col-md-4 col-lg-2 mb-4 glass-background ">
+                                <div class="glass-card">
+                                    <a href="${poster}" target="_blank">
+                                        <img src="${poster}" class="card-img-top" alt="${movie.Title}" onerror="this.onerror=null; this.src='/assets/images/default.png';">
+                                    </a>
+                                    <div class="card-body">
+                                        <h5 class="card-title">${movie.Title}</h5>
+                                        <p class="card-text">Year: ${movie.Year}</p>
+                                        ${generateDownloadLinks(imdbID, movie.Year, movie.Type)}
+                                    </div>
+                                </div>
+                            </div>
+                        `;
+            });
+            moviesHtml += "</div>";
+            resultsContainer.innerHTML = moviesHtml;
+
+            resultsContainer.scrollIntoView({behavior: "smooth"});
+        } else {
+            resultsContainer.innerHTML = '<div class="alert alert-danger">Not found!</div>';
+        }
+    } catch (error) {
+        console.error("Error: ", error);
+        document.getElementById("results").innerHTML = '<div class="alert alert-danger">Error: ' + error.message + "</div>";
+    }
+}
+
+document
+    .getElementById("searchForm")
+    .addEventListener("submit", function (event) {
+        event.preventDefault();
+        const title = document.getElementById("title").value.trim();
+        fetchWithToken(title).then(r => {
+        });
+    });
+
+function generateDownloadLinks(imdbID, year, type) {
+    if (type === "movie") {
+        const originalDownloadLink = `https://tokyo.saymyname.website/Movies/${year}/${imdbID}`;
+        const backupDownloadLink = `https://berlin.saymyname.website/Movies/${year}/${imdbID}`;
+
+        return `
+            <a href="${originalDownloadLink}" class="btn btn-primary mb-2">Download</a><br>
+            <a href="${backupDownloadLink}" class="btn btn-secondary mb-2">Download - Backup Link</a><br>
+        `;
+    } else if (type === "series") {
+        return generateSeriesDownloadLinks(imdbID);
+    }
+    return "";
+}
+
+function generateSeriesDownloadLinks(imdbID) {
+    let seasonsHtml = '<div class="accordion" id="seasonsAccordion">';
+    for (let i = 1; i <= 4; i++) {
+        seasonsHtml += `
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="heading${i}">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${i}" aria-expanded="true" aria-controls="collapse${i}">
+                        فصل ${i}
+                    </button>
+                </h2>
+                <div id="collapse${i}" class="accordion-collapse collapse" aria-labelledby="heading${i}" data-bs-parent="#seasonsAccordion">
+                    <div class="accordion-body">
+                        ${generateQualityLinks(imdbID, i)}
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+    seasonsHtml += "</div>";
+    return seasonsHtml;
+}
+
+function generateQualityLinks(imdbID, season) {
+    let qualityLinks = "";
+    for (let quality = 1; quality <= 4; quality++) {
+        const downloadLink = `https://subtitle.saymyname.website/DL/filmgir/?i=tt${imdbID}&f=${season}&q=${quality}`;
+        qualityLinks += `<a href="${downloadLink}" class="btn btn-success mb-2">${season} - ${quality}</a><br>`;
+    }
+    return qualityLinks;
+}
+
+document.getElementById('searchForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const title = document.getElementById('title').value;
+    const resultsDiv = document.getElementById('results');
+
+    if (title.trim()) {
+        resultsDiv.innerHTML = `Searching: ${title}...`;
+
+        // setTimeout(function () {
+        //     resultsDiv.innerHTML = `Results: ${title}`;
+        // }, 1000);
+    } else {
+        resultsDiv.innerHTML = 'Enter the movie name: ';
+    }
+});
