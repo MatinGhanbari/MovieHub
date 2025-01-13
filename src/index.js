f755d86e4a00da7ee8d29e01cf0dbfb79bf0d5b8
Matin_Ghanbari <ghanbarimatin6@gmail.com>
2025-01-13 23:30:11 +0330

    Add pagination

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-async function fetchWithToken(title) {
+async function fetchWithToken(title, page=1) {
     try {
-        let url = `https://www.omdbapi.com/?apikey=abb7cdf7&s=${encodeURIComponent(title)}`;
+        let url = `https://www.omdbapi.com/?apikey=abb7cdf7&s=${encodeURIComponent(title)}&page=${page}`;
 
         const response = await fetch(url);
         const data = await response.json();
@@ -12,6 +12,7 @@ async function fetchWithToken(title) {
 
             let moviesHtml = '<div class="row">';
             for (const movie of data.Search) {
+                if(movie.Type !== "movie" && movie.Type !== "series") continue;
                 const poster = movie.Poster !== "N/A" ? movie.Poster : "https://raw.githubusercontent.com/MatinGhanbari/MovieHub/refs/heads/main/assets/images/default-cover.jpg";
                 const imdbID = movie.imdbID.replace("tt", "");
 
@@ -37,6 +38,14 @@ async function fetchWithToken(title) {
             moviesHtml += "</div>";
             resultsContainer.innerHTML = moviesHtml;
             resultsContainer.scrollIntoView({behavior: "smooth"});
+
+            let pagination = `<div style="display: flex;justify-content: space-evenly;align-items: center;">`;
+            if(page > 1) pagination += `<p class="page-link" onclick="fetchWithToken('${title}', ${page-1});">Previous Page</p>`;
+            if((page + 1) * 10 < data.totalResults) pagination += `<p class="page-link" onclick="fetchWithToken('${title}', ${page+1});">Next page</p>`;            
+            pagination += `</div>`;
+
+            resultsContainer.innerHTML += pagination;
+            resultsContainer.innerHTML += `<div style="display: flex;justify-content: space-evenly;align-items: center;"><strong>Page ${page} - ${(data.totalResults/10).toFixed()}</strong></div>`;
         } else {
             resultsContainer.innerHTML = '<div class="alert alert-danger">Movie not found!</div>';
         }
@@ -48,17 +57,21 @@ async function fetchWithToken(title) {
 
 
 async function generateDownloadLinks(imdbID, year, type) {
-    if (type === "movie") {
-        const originalDownloadLink = `https://tokyo.saymyname.website/Movies/${year}/${imdbID}/`;
-        const backupDownloadLink = `https://berlin.saymyname.website/Movies/${year}/${imdbID}/`;
-
-        let linksHtml = '';
-        linksHtml += `<a href="${originalDownloadLink}" target="_blank" class="btn btn-primary mb-2">Download</a><br>`;
-        linksHtml += `<a href="${backupDownloadLink}" target="_blank" class="btn btn-secondary mb-2">Backup Link</a><br>`;
-
-        return linksHtml;
-    } else if (type === "series") {
-        return generateSeriesDownloadLinks(imdbID);
+    try{
+        if (type === "movie") {
+            const originalDownloadLink = `https://tokyo.saymyname.website/Movies/${year}/${imdbID}/`;
+            const backupDownloadLink = `https://berlin.saymyname.website/Movies/${year}/${imdbID}/`;
+    
+            let linksHtml = '';
+            linksHtml += `<a href="${originalDownloadLink}" target="_blank" class="btn btn-primary mb-2">Download</a><br>`;
+            linksHtml += `<a href="${backupDownloadLink}" target="_blank" class="btn btn-secondary mb-2">Backup Link</a><br>`;
+    
+            return linksHtml;
+        } else if (type === "series") {
+            return generateSeriesDownloadLinks(imdbID);
+        }
+    }catch(error){
+        return '<div class="alert alert-danger">Movie not found!</div>';
     }
     return "";
 }
